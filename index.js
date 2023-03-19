9eaa18b8c0df7e1f2cdfbf27b185f45619571efd
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-03-19 13:58:52 -0400

    finished lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result;
+
+  if (feet <= 400) {
+    result = "This one is on me!";
+  } else if (feet > 400 && feet <= 2000) {
+    result = "That will be twenty bucks.";
+  } else if (feet > 2000 && feet <= 2500) {
+    result = "I will gladly take your thirty bucks.";
+  } else if (feet > 2500) {
+    result = "No can do.";
+  } 
+
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === "NYC") ?  "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+
+  case "generous":
+    return "Thank you so much.";
+
+  case "not as generous":
+    return "Thank you.";
+  
+  default :
+   return "Bye.";  
+  }
+  }
